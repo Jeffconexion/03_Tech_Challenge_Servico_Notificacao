9a65ffa01791130e9d4d833dbc71b75d6509b420
jeffconexion <jeff.debian2016@gmail.com>
2024-11-13 07:17:48 -0300

    Adicionando valor guid.

--- Service.Notification/Service.Notification/Domain/MessageNotification.cs
+++ Service.Notification/Service.Notification/Domain/MessageNotification.cs
@@ -2,6 +2,7 @@
 {
     public class MessageNotification
     {
+        public Guid? Id { get; set; }
         public string? Name { get; set; }
         public string? Message { get; set; }
         public string? Email { get; set; }
